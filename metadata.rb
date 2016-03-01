5c59c22b437c244bc612b0f0ffb5b93fffaa8db1
Thomas Pike <thwpike@crossroads.org.hk>
2016-03-01 12:21:52 +0800

    Bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'itdept@crossroads.org.hk'
 license          'Apache 2.0'
 description      'Installs and configures Atlassian Confluence'
 long_description 'Installs and configures Atlassian Confluence'
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'apache2',            '>= 3.1.0'
 depends 'java',               '>= 1.31.0'
